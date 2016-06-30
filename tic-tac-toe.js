e1db6c4a7a774693a3cde4af12c970d43fe259d0
Alysia Brown <alysiacbrown@gmail.com>
2016-06-30 16:38:13 -0700

    Added player turns so boxes fill with X or O.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,43 +3,26 @@ function TicTacToe() {
 }
 
 TicTacToe.prototype = {
+}
 
+// Functions for checking boxes
+function checkBox(id, letter) {
+  document.getElementById(id).innerHTML = letter;
+  console.log(letter)
 }
 
 $(document).ready( function() {
-  $( "#square1" ).click(function() {
+  var player = 1
+  console.log(player)
+
+  $( ".checkbox" ).click(function() {
     console.log("You clicked square 1")
+    if (player === 1) {
+      checkBox(this.id, "X")
+      player = 2
+    } else {
+      checkBox(this.id, "O")
+      player = 1
+    }
   });
-
-  $( "#square2" ).click(function() {
-    console.log("You clicked square 2")
-  });
-
-  $( "#square3" ).click(function() {
-    console.log("You clicked square 3")
-  });
-
-  $( "#square4" ).click(function() {
-    console.log("You clicked square 4")
-  });
-
-  $( "#square5" ).click(function() {
-    console.log("You clicked square 5")
-  });
-
-  $( "#square6" ).click(function() {
-    console.log("You clicked square 6")
-  });
-
-  $( "#square7" ).click(function() {
-    console.log("You clicked square 7")
-  });
-
-  $( "#square8" ).click(function() {
-    console.log("You clicked square 8")
-  });
-
-  $( "#square9" ).click(function() {
-    console.log("You clicked square 9")
-  });
-})
+});
