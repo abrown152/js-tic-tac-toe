92ebda763800cee13f4b0884fa8862156ed02f55
Alysia Brown <alysiacbrown@gmail.com>
2016-06-30 14:39:30 -0700

    Created board outline and clickable squares.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,5 +3,43 @@ function TicTacToe() {
 }
 
 TicTacToe.prototype = {
-  
+
 }
+
+$(document).ready( function() {
+  $( "#square1" ).click(function() {
+    console.log("You clicked square 1")
+  });
+
+  $( "#square2" ).click(function() {
+    console.log("You clicked square 2")
+  });
+
+  $( "#square3" ).click(function() {
+    console.log("You clicked square 3")
+  });
+
+  $( "#square4" ).click(function() {
+    console.log("You clicked square 4")
+  });
+
+  $( "#square5" ).click(function() {
+    console.log("You clicked square 5")
+  });
+
+  $( "#square6" ).click(function() {
+    console.log("You clicked square 6")
+  });
+
+  $( "#square7" ).click(function() {
+    console.log("You clicked square 7")
+  });
+
+  $( "#square8" ).click(function() {
+    console.log("You clicked square 8")
+  });
+
+  $( "#square9" ).click(function() {
+    console.log("You clicked square 9")
+  });
+})
