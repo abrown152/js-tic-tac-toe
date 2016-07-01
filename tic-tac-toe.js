e89e83130608f55abc23d730c851ca3e2c5dab3e
Alysia Brown <alysiacbrown@gmail.com>
2016-07-01 15:40:35 -0700

    Added hasWon function. If only it worked...

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,27 +2,67 @@ function TicTacToe() {
 
 }
 
-TicTacToe.prototype = {
+TicTacToe.hasWon = function() {
+  // Check for a winner
+  var square1value = document.getElementById("square1").innerHTML
+  var square2value = document.getElementById("square2").innerHTML
+  var square3value = document.getElementById("square3").innerHTML
+  var square4value = document.getElementById("square4").innerHTML
+  var square5value = document.getElementById("square5").innerHTML
+  var square6value = document.getElementById("square6").innerHTML
+  var square7value = document.getElementById("square7").innerHTML
+  var square8value = document.getElementById("square8").innerHTML
+  var square9value = document.getElementById("square9").innerHTML
+
+  if ((squarevalue1 === squarevalue2) && (squarevalue2 === squarevalue3)) {
+    return true
+  } else if ((squarevalue4 === squarevalue5) && (squarevalue5 === squarevalue6)) {
+    return true
+  } else if ((squarevalue7 === squarevalue8) && (squarevalue8 === squarevalue9)) {
+    return true
+  } else if ((squarevalue1 === squarevalue4) && (squarevalue4 === squarevalue7)) {
+    return true
+  } else if ((squarevalue2 === squarevalue5) && (squarevalue5 === squarevalue8)) {
+    return true
+  } else if ((squarevalue3 === squarevalue6) && (squarevalue6 === squarevalue9)) {
+    return true
+  } else if ((squarevalue1 === squarevalue5) && (squarevalue5 === squarevalue9)) {
+    return true
+  } else if ((squarevalue3 === squarevalue5) && (squarevalue5 === squarevalue7)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Functions for checking boxes
 function checkBox(id, letter) {
   document.getElementById(id).innerHTML = letter;
-  console.log(letter)
 }
 
 $(document).ready( function() {
+  // The player variable keeps track of whose turn it is
   var player = 1
-  console.log(player)
 
+  // Uses player variable to determine whether to mark X or O and which box to mark
   $( ".checkbox" ).click(function() {
-    console.log("You clicked square 1")
-    if (player === 1) {
-      checkBox(this.id, "X")
-      player = 2
+    if (document.getElementById(this.id).innerHTML !== ("X" || "O")) {
+      // Trying to prevent multiple plays on the same square but this isn't working
+      if (player === 1) {
+        checkBox(this.id, "X")
+        player = 2
+        if (TicTacToe.hasWon() === true) {
+          alert("We have a winner^_^")
+        }
+      } else {
+        checkBox(this.id, "O")
+        player = 1
+        if (TicTacToe.hasWon() === true) {
+          alert("We have a winner^_^")
+        }
+      }
     } else {
-      checkBox(this.id, "O")
-      player = 1
+      alert("This box has already been played. Please choose another.")
     }
   });
 });
